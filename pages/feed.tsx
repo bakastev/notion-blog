4d31c2f43f41af2c797a76504a78891681124551
bakastev <127332568+bakastev@users.noreply.github.com>
2023-03-15 16:09:16 +0100

    Update feed.tsx

--- pages/feed.tsx
+++ pages/feed.tsx
@@ -65,9 +65,9 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
     )
     const publishedTime = getPageProperty<number>('Published', block, recordMap)
     const date = lastUpdatedTime
-      ? new Date(lastUpdatedTime)
+      ? new Date(lastUpdatedTime).toLocaleDateString('de-DE')
       : publishedTime
-      ? new Date(publishedTime)
+      ? new Date(publishedTime).toLocaleDateString('de-DE')
       : undefined
     const socialImageUrl = getSocialImageUrl(pageId)
 
