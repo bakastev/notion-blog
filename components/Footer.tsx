5ef6bbf928ed74812b103c4433a5e9de9571158a
bakastev <127332568+bakastev@users.noreply.github.com>
2023-03-15 09:47:45 +0100

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -87,17 +87,7 @@ export const FooterImpl: React.FC = () => {
           </a>
         )}
 
-        {config.github && (
-          <a
-            className={styles.github}
-            href={`https://github.com/${config.github}`}
-            title={`GitHub @${config.github}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaGithub />
-          </a>
-        )}
+       
 
         {config.linkedin && (
           <a
