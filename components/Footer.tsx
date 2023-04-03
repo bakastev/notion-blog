13223d68dae3e4bfeafef3c0a55b385933e1d03a
bakastev <127332568+bakastev@users.noreply.github.com>
2023-04-03 15:45:53 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -34,7 +34,7 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2023 - Menschen.Buchen.Fotografen</div>
+      <div className={styles.copyright}>Copyright 2023 - <a href='https://menschen-buchen-fotografen.de'Menschen.Buchen.Fotografen</a></div>
 
       <div className={styles.settings}>
         {hasMounted && (
